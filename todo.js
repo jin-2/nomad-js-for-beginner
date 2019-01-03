438f9aabfd64349111393d35b1b7ff4ac4e2f1c8
jin-2 <jmakeaz@gmail.com>
2019-01-03 14:58:04 +0900

    중복되는 printTodo 내용 수정

--- todo.js
+++ todo.js
@@ -5,29 +5,19 @@ const todoList = document.querySelector('.todo-list');
 const TODO_LS = 'toDos';
 let todoData = [];
 
-function saveTodo(data) {
-    localStorage.setItem(TODO_LS, JSON.stringify(data));
-}
-
-function submitHandle(event) {
-    event.preventDefault();
-    const todoText = todoInput.value;
-    printTodo(todoText);
-    saveTodo(todoData);
-    todoInput.value = '';
-}
-
 function deleteTodo(event) {
     const btn = event.target;
     const listItem = btn.parentNode;
     listItem.remove();
     var cleanedData = todoData.filter(function (todo) {
-        console.log(todo.id !== listItem.id);
         return todo.id !== parseInt(listItem.id);
     });
-    console.log(cleanedData);
     todoData = cleanedData;
-    saveTodo(cleanedData);
+    saveTodo();
+}
+
+function saveTodo() {
+    localStorage.setItem(TODO_LS, JSON.stringify(todoData));
 }
 
 function printTodo(todoVal) {
@@ -36,18 +26,29 @@ function printTodo(todoVal) {
     const text = document.createElement('span');
     const todoId = todoData.length + 1;
     delBtn.innerText = 'Delete';
+    delBtn.setAttribute('type', 'button');
+    delBtn.addEventListener('click', deleteTodo);
     text.innerText = todoVal;
     li.appendChild(delBtn);
     li.appendChild(text);
     li.id = todoId;
     todoForm.appendChild(li);
-    delBtn.addEventListener('click', deleteTodo);
     const todoItem = {
         id: todoId,
         text: todoVal
     };
     todoData.push(todoItem);
+    saveTodo();
 }
+
+function submitHandle(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const todoText = todoInput.value;
+    printTodo(todoText);
+    todoInput.value = '';
+}
+
 
 function loadTodo() {
     const loadedTodo = localStorage.getItem(TODO_LS);
